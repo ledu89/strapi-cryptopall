915ce980d76f0c80f66542723ad628d8a2f860b6
ledu89 <dninkov89@gmail.com>
2024-10-29 17:19:19 +0100

    cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,12 +1,20 @@
-module.exports = [
-  'strapi::logger',
-  'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
-  'strapi::poweredBy',
-  'strapi::query',
-  'strapi::body',
-  'strapi::session',
-  'strapi::favicon',
-  'strapi::public',
-];
+(module.exports = [
+  "strapi::logger",
+  "strapi::errors",
+  "strapi::security",
+  "strapi::cors",
+  "strapi::poweredBy",
+  "strapi::query",
+  "strapi::body",
+  "strapi::session",
+  "strapi::favicon",
+  "strapi::public",
+]),
+  {
+    name: "strapi::security",
+    config: {
+      cors: {
+        origin: ["https://tvoj-vercel-url.vercel.app"],
+      },
+    },
+  };
