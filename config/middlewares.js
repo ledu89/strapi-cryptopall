c4849ed41249813c346e89b8836741848cb996ee
ledu89 <dninkov89@gmail.com>
2024-10-30 16:47:22 +0100

    refactoring

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,44 +1,8 @@
-// module.exports = [
-//   "strapi::logger",
-//   "strapi::errors",
-//   "strapi::security",
-//   "strapi::cors",
-//   "strapi::poweredBy",
-//   "strapi::query",
-//   "strapi::body",
-//   "strapi::session",
-//   "strapi::favicon",
-//   "strapi::public",
-// ];
 module.exports = [
   "strapi::logger",
   "strapi::errors",
-  {
-    name: "strapi::security",
-    config: {
-      contentSecurityPolicy: {
-        useDefaults: true,
-        directives: {
-          "connect-src": [
-            "'self'",
-            "https://strapi-cryptopall.onrender.com",
-            "https://cryptopall.vercel.app",
-          ],
-        },
-      },
-    },
-  },
-  {
-    name: "strapi::cors",
-    config: {
-      enabled: true,
-      origin: [
-        "https://cryptopall.vercel.app",
-        "https://strapi-cryptopall.onrender.com",
-      ],
-      headers: "*",
-    },
-  },
+  "strapi::security",
+  "strapi::cors",
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
