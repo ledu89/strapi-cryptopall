c6d72d1074c771d035ab6357a759e89c08dd723a
ledu89 <dninkov89@gmail.com>
2024-10-30 15:45:25 +0100

    cors

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,8 +1,44 @@
+// module.exports = [
+//   "strapi::logger",
+//   "strapi::errors",
+//   "strapi::security",
+//   "strapi::cors",
+//   "strapi::poweredBy",
+//   "strapi::query",
+//   "strapi::body",
+//   "strapi::session",
+//   "strapi::favicon",
+//   "strapi::public",
+// ];
 module.exports = [
   "strapi::logger",
   "strapi::errors",
-  "strapi::security",
-  "strapi::cors",
+  {
+    name: "strapi::security",
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          "connect-src": ["'self'", "https:"],
+          "img-src": ["'self'", "data:", "blob:", "https:"],
+          "media-src": ["'self'", "data:", "blob:", "https:"],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
+  {
+    name: "strapi::cors",
+    config: {
+      origin: [
+        "https://cryptopall.vercel.app",
+        "https://cryptopall-brhbcr7d1-ledus-projects.vercel.app",
+      ],
+      methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+      headers: ["Content-Type", "Authorization"],
+      keepHeadersOnError: true,
+    },
+  },
   "strapi::poweredBy",
   "strapi::query",
   "strapi::body",
