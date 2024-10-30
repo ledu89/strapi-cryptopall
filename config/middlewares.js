ab881f2fbe9be1df73ecb297f6c3715f984ff4ea
ledu89 <dninkov89@gmail.com>
2024-10-30 16:54:59 +0100

    strapi

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,3 +1,15 @@
+// module.exports = [
+//   "strapi::logger",
+//   "strapi::errors",
+//   "strapi::security",
+//   "strapi::cors",
+//   "strapi::poweredBy",
+//   "strapi::query",
+//   "strapi::body",
+//   "strapi::session",
+//   "strapi::favicon",
+//   "strapi::public",
+// ];
 module.exports = [
   "strapi::logger",
   "strapi::errors",
@@ -9,4 +21,13 @@ module.exports = [
   "strapi::session",
   "strapi::favicon",
   "strapi::public",
+  {
+    name: "strapi::cors",
+    config: {
+      origin: ["*"], // Adjust this to your frontend URL for production
+      methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+      allowedHeaders: ["Content-Type", "Authorization"],
+      credentials: true,
+    },
+  },
 ];
